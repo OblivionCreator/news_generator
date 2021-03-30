00bbb0cfd04a4babe246b617d241ef5eb3e72846
OblivionCreator <oblivioncreator@outlook.com>
2021-03-30 19:18:22 +0100

    added useverb - allows you to determine whether you want to use the verb found in the message or just replace it by 'IS' by default.
    Also added more headline variations.

--- nlp.py
+++ nlp.py
@@ -1,7 +1,7 @@
 import language_processing
 import random
 
-def generate(username, text):
+def generate(username, text, useverb=True):
     noun_phr, verb, adj = language_processing.tokenize(text) # Converts input text into tokens.
     print(noun_phr, verb, adj)
 
@@ -31,10 +31,19 @@ def generate(username, text):
     rand_un_adj = random.choice(un_adj)
     rand_un_verb = random.choice(un_verb)
 
-    return headlineGenerator(username, randPhrase, rand_un_adj, rand_un_verb)
+    return headlineGenerator(username, randPhrase, rand_un_adj, rand_un_verb, useverb)
 
-def headlineGenerator(name, phrase, adjective, verb):
+def headlineGenerator(name, phrase, adjective, verb='', useverb=True):
 
-    headline = f"BREAKING NEWS: {name.upper()} thinks that {phrase.upper()} {verb.upper()} {adjective.upper()}!"
+    if useverb == True:
+        v_ = verb
+    else:
+        v_ = 'IS'
+
+    c_list = ["thinks that", "said that", "stated that", "strongly believes that", "does not believe that", "made it public that they think that", "determined that", "believes that", "did not understand that"]
+
+    c_ = c_list[random.randint(0, len(c_list))]
+
+    headline = f"BREAKING NEWS: {name.upper()} {c_} {phrase.upper()} {v_.upper()} {adjective.upper()}!"
 
     return headline
